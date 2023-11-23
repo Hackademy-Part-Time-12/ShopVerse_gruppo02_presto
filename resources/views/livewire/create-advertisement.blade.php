170f40b85f3ca3037027f7f49149b634f21adf32
Salvatore Covelli <sasycove.sc@gmail.com>
2023-11-23 13:36:15 +0100

    aggiunta componente livewire per il form

--- resources/views/livewire/create-advertisement.blade.php
+++ resources/views/livewire/create-advertisement.blade.php
@@ -1,34 +1,36 @@
 <section class="sezione2 tutto mt-5 pt-5">
-    <link rel="stylesheet" href="register.css">
+
     <div class="form-box">
         <div class="form-value">
-            <form class="" action="{{ route('register') }}" method="POST">
+            <form class="" wire:submit.privent="store">
                 @csrf
                 <h2 class="h2">Inserisci Annuncio</h2>
+                @if (session('PostCreate'))
+                    <div class="alert alert-success ">{{ session('PostCreate') }}</div>
+                @endif
                 {{-- Form title --}}
                 <div class="inputbox">
-                    <input type="text" wire:model.live='title' id="title" value="{{ old('title') }}">
-
+                    <input type="text" wire:model.live='title' id="title">
                     <label for="title">Titolo annuncio</label>
                     @error('title')
                         <p class=" text-danger ">{{ $message }}</p>
                     @enderror
                 </div>
 
-                {{-- Form password --}}
+                {{-- Form price --}}
                 <div class="inputbox">
-                <ion-icon name="lock-closed-outline"><i class="fa-solid fa-eye see" onclick='seepassword()' style="color: #0b5fef;"></i></ion-icon>
-                <input type="password" id="password" name="password">
-                <label for="password">Password</label>
-              </div>
-                 {{-- Form description  --}}
-                 <div class="inputbox">
-                    <label  for="description"></label>
+                    <ion-icon name="lock-closed-outline"><i class="fa-solid fa-eye see"
+                            {{-- onclick='seepassword()' style="color: #0b5fef;" --}}></i></ion-icon>
+                    <input type="number" id="number" wire:model.live="number">
+                    <label for="number">Prezzo</label>
+                </div>
 
-                    <textarea class="form-control border-0 " placeholder='Descrizione' id="description" cols="15" rows="6">{{ old('description') }}</textarea>
-
-
-                    @error('description')
+                {{-- Form body  --}}
+                <div class="inputbox">
+                    <label for="body"></label>
+                    <textarea class="form-control border-0 " wire:model.live="body" placeholder='Descrizione' id="body" cols="15"
+                        rows="6"></textarea>
+                    @error('body')
                         <p class="text-danger ">{{ $message }}</p>
                     @enderror
 
