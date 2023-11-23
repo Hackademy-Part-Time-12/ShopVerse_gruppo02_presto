2f6c0fe1bcf8ef8bfbced00ebb23e66b3f27a3aa
Lorenzo Montorsi <lorenzomontorsi@Mac-mini-di-Lorenzo.local>
2023-11-23 21:11:19 +0100

    modificato il footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -25,10 +25,10 @@
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-6 col-sm-7">
-                    <p class="mb-0 f_400 text-left">© 2023 - ShopVerse - </p>
+                    <p class="mb-0 f_400 text-left fw-bold copyright" style="color:white; margin-left: -40px; margin-top: -15px;">© 2023 - ShopVerse - </p>
                 </div>
                 <div class="col-lg-6 col-sm-5 text-right d-flex align-items-end justify-content-end">
-                    <p class="mb-0">
+                    <p class="mb-0 fw-bold creatori" style="color: white;">
                         Made with <i class="fa-solid fa-heart" style="color: #fe0101;"></i>
                         by <a href="http://shopverse.com" target="_blank"> Lorenzo Montorsi - Salvatore Covelli - Giulia Gaudimundo</a>
                     </p>
@@ -36,4 +36,6 @@
             </div>
         </div>
     </div>
+
+    
 </footer>
