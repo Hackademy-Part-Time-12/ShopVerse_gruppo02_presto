76545ac633f5cdddd1e0216f6b34ba47a0293f1e
Giulia Gaudimundo <giulia.gaudimundo95@gmail.com>
2023-11-24 17:10:23 +0100

    inserimento_Favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@200&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="\Media\favicon.ico">
 
     {{-- Scafolding --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
