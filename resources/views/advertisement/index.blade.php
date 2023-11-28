b1e38908edb2b63660303ba4b2e8158d5e63261a
Salvatore Covelli <sasycove.sc@gmail.com>
2023-11-28 13:15:35 +0100

    modifiche

--- resources/views/advertisement/index.blade.php
+++ resources/views/advertisement/index.blade.php
@@ -33,6 +33,7 @@
                                     </div>
                                 </div>
                             @endforeach
+                            {{ $advertisement->links() }}
                         </div>
                     </div>
                 </div>
