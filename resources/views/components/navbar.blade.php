3b049176ce6c571aa81dd20a94895e3b48d75c78
Lorenzo Montorsi <lorenzomontorsi@Mac-mini-di-Lorenzo.local>
2023-11-28 22:05:43 +0100

    navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <div class="container-fluid w-100 pt-0 mt-0 navbar z-3" role="navigation">
     <div class="row text-center w-100 align-items-center g-0">
-        <div class="col-lg-5 col-md-5 d-none d-lg-block d-md-block d-xs-none ms-auto">
+        <div class="col-lg-5 col-md-5 d-none d-lg-block d-md-block d-xs-none ms-auto contenitore">
             <div class="d-flex justify-content-around border-top border-bottom border-dark linea">
                 <div class="customLink"><a class="d-inline-block p-3" href="{{ Route('home') }}">Home</a></div>
                 <div class="customLink"><a class="d-inline-block p-3" href="#">Product</a></div>
@@ -10,7 +10,7 @@
         <div class="col-lg-2 col-md-2 mx-auto" style="max-width:120px;">
           
         </div>
-        <div class="col-lg-5 col-md-5 d-none d-lg-block d-md-block d-xs-none me-auto">
+        <div class="col-lg-5 col-md-5 d-none d-lg-block d-md-block d-xs-none me-auto contenitore2">
             <div class="d-flex justify-content-around border-top border-bottom border-dark linea">
                 <div class="customLink"><a class="d-inline-block p-3" href="#">Company</a></div>
                 <div class="customLink"><a class="d-inline-block p-3" href="#">Services</a></div>
