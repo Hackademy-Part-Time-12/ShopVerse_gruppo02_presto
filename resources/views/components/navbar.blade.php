0bf13e4e242c31b29c673ea9b541ed5c6426e715
Salvatore Covelli <sasycove.sc@gmail.com>
2023-11-29 17:38:20 +0100

    modifiche apportate

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,20 +4,24 @@
                 {{-- Sezione SX --}}
                 <div class="col-lg-5 col-md-5 d-none d-lg-block d-md-block d-xs-none ms-auto">
                     <div class="d-flex justify-content-around border-top border-bottom border-dark linea">
+                        {{-- Home --}}
                         <div class="customLink">
-                            <a class="d-inline-block p-3" href="{{ Route('home') }}">Home</a>
+                            <a class="d-inline-block p-3" href="{{ Route('home') }}"><b class="navbarColor">Home</b> </a>
                         </div>
-
+                        {{-- Annunci --}}
                         <div class="customLink">
                             <a class="d-inline-block p-3" href="{{ route('advertisement.index') }}"><b
                                     class="navbarColor">Annunci</b></a>
                         </div>
-
+                        {{-- inserisci annunci --}}
                         <div class="customLink">
-                            <a class="d-inline-block p-3" href="{{route('advertisement.create') }}">Inserisci Annunci</a>
+                            <a class="d-inline-block p-3" href="{{ route('advertisement.create') }}"><b
+                                    class="navbarColor">Inserisci Annunci</b> </a>
                         </div>
+
                     </div>
                 </div>
+
                 {{-- Sezione centrale --}}
                 <div class="col-lg-2 col-md-2 mx-auto" style="max-width:120px;">
                     {{-- foto gestita nel leyout --}}
@@ -27,7 +31,9 @@
                 {{-- Sezione DX --}}
                 <div class="col-lg-5 col-md-5 d-none d-lg-block d-md-block d-xs-none me-auto">
                     <div class="d-flex justify-content-around border-top border-bottom border-dark linea">
+
                         {{-- Sezione Categorie --}}
+                        
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle " href="#" id="categoriesDropdown" role="button"
                                 data-bs-toggle="dropdown" aria-expanded="false">
@@ -45,14 +51,11 @@
                             </ul>
                         </li>
 
+                        {{-- Contattaci --}}
                         <div class="customLink">
-                            <a class="d-inline-block p-3" href="#">Chi siamo</a>
+                            <a class="d-inline-block p-3" href="#"><b class="navbarColor">Contattaci</b> </a>
 
                         </div>
-
-
-
-
                         {{-- Sezione visione loggato --}}
                         <li class="nav-item dropdown fw-bold">
                             <a class="nav-link dropdown-toggle fw-bold " href="#" id="navbarDropdownMenuLink"
@@ -91,38 +94,57 @@
             <div class="row text-center w-100 align-items-center g-0">
                 <div class="col-lg-5 col-md-5 d-none d-lg-block d-md-block d-xs-none ms-auto">
                     <div class="d-flex justify-content-around border-top border-bottom border-dark linea">
-                        <div class="customLink"><a class="d-inline-block p-3" href="{{ Route('home') }}">Home</a></div>
+                        {{-- Home --}}
+                        <div class="customLink">
+                            <a class="d-inline-block p-3" href="{{ Route('home') }}"><b class="navbarColor">Home</b> </a>
+                        </div>
+                        {{-- Annunci --}}
                         <div class="customLink"><a class="d-inline-block p-3"
-                                href="{{ route('advertisement.index') }}">Annunci</a></div>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle " href="#" id="categoriesDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                <b class="navbarColor">Categorie</b></a>
+                                href="{{ route('advertisement.index') }}"><b class="navbarColor">Annunci</b> </a>
+                        </div>
+                        {{-- Categorie --}}
 
-                            <ul class="dropdown-menu" aria-labelledby="categoriesDropdown">
-                                @foreach ($categories as $category)
-                                    <li><a class="dropdown-item  "
-                                            href=" {{ route('categoryShow', compact('category')) }} ">{{ $category->name }}</a>
-                                    </li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle " href="#" id="categoriesDropdown" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    <b class="navbarColor">Categorie</b></a>
+
+                                <ul class="dropdown-menu" aria-labelledby="categoriesDropdown">
+                                    @foreach ($categories as $category)
+                                        <li><a class="dropdown-item  "
+                                                href=" {{ route('categoryShow', compact('category')) }} ">{{ $category->name }}</a>
+                                        </li>
+                                        <li>
+                                            <hr class="dropdown-divider">
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </li>
+
+
+
+
+
                     </div>
                 </div>
+
                 {{-- Sezione centro --}}
                 <div class="col-lg-2 col-md-2 mx-auto" style="max-width:120px;">
                     {{-- immagine gestita con leyout --}}
 
                 </div>
+
                 {{-- Sezione dx --}}
                 <div class="col-lg-5 col-md-5 d-none d-lg-block d-md-block d-xs-none me-auto">
                     <div class="d-flex justify-content-around border-top border-bottom border-dark linea">
-                        <div class="customLink"><a class="d-inline-block p-3" href="#">Contattaci</a></div>
+
+                        {{-- Contattaco --}}
+                        <div class="customLink">
+                            <a class="d-inline-block p-3" href="#"><b class="navbarColor">Contattaci</b> </a>
+                        </div>
+
                         {{-- Benvenuto utente --}}
-                        <li class="nav-item dropdown  ">
+                        <li class="nav-item dropdown">
                             <a class="nav-link fw-bold dropdown-toggle navbarColor" href="#"
                                 id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 <b class="navbarColor">Benvenuto Utente</b>
@@ -136,6 +158,7 @@
                                 <!-- <li><a class="dropdown-item" href="#">Contact us</a></li> -->
                             </ul>
                         </li>
+
                     </div>
                 </div>
             </div>
