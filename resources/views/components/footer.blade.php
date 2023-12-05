c8220798f0488f248cdb31365ada6936e853341d
Lorenzo Montorsi <lorenzomontorsi@Mac-mini-di-Lorenzo.local>
2023-12-05 21:28:14 +0100

    Revert "sistemato i media query di nav e foot"
    This reverts commit db243c9e6845cc63fcb242fc2323bfe7166ab512.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,75 @@
-<!-- Footer -->
-<footer class="bg-primary text-center text-white">
-    <!-- Grid container -->
-    <div class="container d-flex justify-content-between  p-4">
 
+<footer class="new_footer_area bg_color pie  " >
+    <div class="f_widget company_widget wow fadeInLeft  " data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInLeft;">
+        <h3 class="f-title f_600 t_color f_size_18 h3 d-none">Get in Touch</h3>
+        <p class="d-none">Don’t miss any updates of our new templates and extensions.!</p>
+        <form action="#" class="f_subscribe_two mailchimp d-none" method="post" novalidate="true" _lpchecked="1">
+            <!-- Contenuto del form -->
+        </form>
+        <div class="col-lg-3 col-md-6 d-none">
+            <div class="f_widget social-widget pl_70 wow fadeInLeft" data-wow-delay="0.8s" style="visibility: visible; animation-delay: 0.8s; animation-name: fadeInLeft;">
+                <h3 class="f-title f_600 t_color f_size_18 h3">Team Solutions</h3>
+                <div class="f_social_icon">
+                    <a href="#" class="fab fa-facebook"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                    <a href="#" class="fab fa-pinterest"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer_bg" data-route="{{route('become.revisor')}}" id="button-74" style="height: 100px; ">
+        <div class="footer_bg_one"></div>
+        <div class="footer_bg_two"></div>
+    </div>
+    <div class="footer_bottom">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-4 col-sm-7">
+                    <p class="mb-0 f_400 text-left fw-bold copyright" style="color:white; margin-left: -40px; margin-top: -15px;">© 2023 - ShopVerse - </p>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <p class="mb-0 f_400 text-left fw-bold"> Vuoi lavorare con noi? </p>
+                    <a href="{{route('become.revisor')}}" class="text-decoration-none" data-route="{{route('become.revisor')}}">
+                    <button class="button-74" type="button">Diventa revisore</button>
+</a>
+                </div>
+                <div class="col-lg-4 col-sm-6 text-right d-flex align-items-end justify-content-end z-3">
+                    <p class="mb-0 fw-bold creatori" style="color: white;">
+                        Made with <i class="fa-solid fa-heart" style="color: #fe0101;"></i>
+                        by <a href="http://shopverse.com" target="_blank"> Lorenzo Montorsi - Salvatore Covelli - Giulia Gaudimundo</a>
+                    </p>
+                </div>
+                <!--Grid row-->
+            </form>
+        </section>
+        <!-- Section: Form -->
+
+
+        <!-- Section: Text -->
+
+
+            <div class="col-4">
+                <p class="mb-0 f_400 text-left fw-bold"> Vuoi lavorare con noi? </p>
+                <a href="{{ route('become.revisor') }}" class="text-decoration-none"><button type="submit"
+                        class="button-53"> Diventa revisore</button></a>
+            </div>
+
+
+
+
+
+        <!-- Section: Text -->
+
+
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        © 2023 Copyright:Lorenzo Montorsi-Salvatore Covelli-Giulia Gaudimundo-Alessio Cannella
+
+    </div>
 
         <!-- Section: Form -->
         <section class=" col-6">
