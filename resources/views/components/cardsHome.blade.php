a5f7d758ce5e40aa3c8df647a0d446e410f5f92d
Salvatore Covelli <sasycove.sc@gmail.com>
2023-12-11 22:07:27 +0100

    modifiche

--- resources/views/components/cardsHome.blade.php
+++ resources/views/components/cardsHome.blade.php
@@ -2,7 +2,7 @@
 
 
             <div class="col-12 col-md-3 shadow2 mx-1 product-grid my-4">
-                <div class="product-image mx-2">
+                <div class="product-image p-3">
                   <img class="pic-1 mt-1 rounded image" src="{{!$advertisement->images()->get()->isEmpty() ? $advertisement->images()->first()->getUrl(300,200) :'https://picsum.photos/200/300'}}">
                     <span class="product-discount-label">{{ $advertisement->created_at->format('d/m/y') }}</span>
                     <ul class="product-links">
